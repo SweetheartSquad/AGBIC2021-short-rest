fe176ecc1e261d83fe78224573b97d681790c74b
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-08-18 01:07:15 -0400

    keep dead at back when shuffling

--- src/assets/cards.js
+++ src/assets/cards.js
@@ -13,7 +13,11 @@
 		effect(scene) {
 			const original = scene.party.slice();
 			do {
-				scene.party.sort(() => Math.random() - 0.5);
+				scene.party.sort((a, b) => {
+					if (a.health <= 0 && b.health > 0) return -1;
+					if (a.health > 0 && b.health <= 0) return 1;
+					return Math.random() - 0.5;
+				});
 			} while (scene.party.every((i, idx) => i === original[idx]));
 		},
 	},
