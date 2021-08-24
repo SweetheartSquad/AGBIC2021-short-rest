017f40bc26e3f69336a21ba60862d85e2456823a
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-08-24 01:14:15 -0400

    replace camp fire animation with full camp animation

--- src/Camp.ts
+++ src/Camp.ts
@@ -2,7 +2,6 @@ import { Sprite } from 'pixi.js';
 import { GameObject } from './GameObject';
 import { Animator } from './Scripts/Animator';
 import { Display } from './Scripts/Display';
-import { size } from './size';
 import { tex } from './utils';
 
 export class Camp extends GameObject {
@@ -13,26 +12,20 @@ export class Camp extends GameObject {
 	constructor() {
 		super();
 		const sprBg = new Sprite(tex('camp_bg'));
-		const sprFire = new Sprite(tex('camp_unlit'));
-		sprFire.anchor.x = 0.5;
-		sprFire.anchor.y = 1.0;
-		sprFire.x = size.x / 2;
-		sprFire.y = size.y * 0.75;
 		const camp = new GameObject();
 		this.display = new Display(camp);
-		this.animator = new Animator(camp, { spr: sprFire });
+		this.animator = new Animator(camp, { spr: sprBg });
 		camp.scripts.push(this.display);
 		camp.scripts.push(this.animator);
 		camp.init();
 		this.display.container.addChild(sprBg);
-		this.display.container.addChild(sprFire);
 	}
 
 	light() {
-		this.animator.setAnimation('camp_lit');
+		this.animator.setAnimation(tex('camp_bg_lit').textureCacheIds[0]);
 	}
 
 	douse() {
-		this.animator.setAnimation('camp_unlit');
+		this.animator.setAnimation(tex('camp_bg').textureCacheIds[0]);
 	}
 }
